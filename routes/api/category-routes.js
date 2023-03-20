efcd2c2ceb53db0516055fa4362bdef239a1fe41
mikafeng <mikafengler@gmail.com>
2023-03-19 23:31:59 -0400

    add get and delete routes to product

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,7 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product}]
+      // include: [{ model: Product}]
     });
     res.status(200).json(categoryData);
     } catch(err) {
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product}],
+      // include: [{ model: Product}],
     });
 
     if (!categoryData) {
